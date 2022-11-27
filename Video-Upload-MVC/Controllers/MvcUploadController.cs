7d0ad21349142b4e5338a4df6958a7c64a0883b1
Jefferson Veiga <gelsvsampa@hotmail.com>
2022-11-27 09:06:05 -0300

    Prepara o método post para salvar arquivo efetuado upload pelo client no servidor

---
+++ Video-Upload-MVC/Controllers/MvcUploadController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Video_Upload_MVC.Controllers
+{
+    public class MvcUploadController : Controller
+    {
+        // GET: MvcUpload
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase videoFile)
+        {
+            if (videoFile != null)
+            {
+                string fileName = Path.GetFileName(videoFile.FileName);
+                if(videoFile.ContentLength < 104857600)
+                {
+                    videoFile.SaveAs(Server.MapPath($"~/VideoFiles/{fileName}"));
+                }
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Video-Upload-MVC/Models/Upload.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Video_Upload_MVC.Models
+{
+    public class Upload
+    {
+        public string Vname { get; set; }
+        public string Vpath { get; set; }
+    }
+}
